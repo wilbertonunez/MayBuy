1be21a7e8b808541c0cf14404739214a17b906d3
wilbertonunez <wilberto.nunez@gmail.com>
2018-02-09 17:04:53 -0500

    Just setting up web service that will be wrapper for store API's

---
+++ src/test/java/hello/GreetingTest.java
@@ -0,0 +1,14 @@
+package hello;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GreetingTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
